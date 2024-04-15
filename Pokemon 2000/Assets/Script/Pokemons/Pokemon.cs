c5562363a8de9961653c1c73bdb8d6f95d969f4a
G4bS2 <gabrielnleal011@gmail.com>
2024-04-15 16:01:25 -0300

    vamoooooooo

--- Pokemon 2000/Assets/Script/Pokemons/Pokemon.cs
+++ Pokemon 2000/Assets/Script/Pokemons/Pokemon.cs
@@ -4,37 +4,54 @@ using UnityEngine;
 
 public class Pokemon 
 {
-    PokemonBase _base;
-    int level;
+   public PokemonBase Base { get; set; }
+    public int Level { get; set; }
+
+    public int HP { get; set; }
+
+    public List<Move> Moves { get; set; }
+
 
     public Pokemon(PokemonBase pBase, int pLevel)
     {
-        _base = pBase;
-        level = pLevel;    
+        Base = pBase;
+        Level = pLevel;
+        HP = MaxHp;
+
+        //gerador de movimentos
+        Moves = new List<Move>();
+        foreach (var move in Base.LearnableMoves)
+        {
+            if (move.Level <= Level)
+                Moves.Add(new Move(move.Base));
+
+            if (Moves.Count >= 4)
+                break;
+        }
     }
 
     public int Attack
     {
-        get { return Mathf.FloorToInt((_base.Attack * level) / 100f) + 5; }
+        get { return Mathf.FloorToInt((Base.Attack * Level) / 100f) + 5; }
     }
     public int defense
     {
-        get { return Mathf.FloorToInt((_base.Attack * level) / 100f) + 5; }
+        get { return Mathf.FloorToInt((Base.Attack * Level) / 100f) + 5; }
     }
     public int SpAttack
     {
-        get { return Mathf.FloorToInt((_base.Attack * level) / 100f) + 5; }
+        get { return Mathf.FloorToInt((Base.Attack * Level) / 100f) + 5; }
     }
     public int SpDefense
     {
-        get { return Mathf.FloorToInt((_base.Attack * level) / 100f) + 5; }
+        get { return Mathf.FloorToInt((Base.Attack * Level) / 100f) + 5; }
     }
     public int Speed
     {
-        get { return Mathf.FloorToInt((_base.Attack * level) / 100f) + 5; }
+        get { return Mathf.FloorToInt((Base.Attack * Level) / 100f) + 5; }
     }
     public int MaxHp
     {
-        get { return Mathf.FloorToInt((_base.Attack * level) / 100f) + 10; }
+        get { return Mathf.FloorToInt((Base.Attack * Level) / 100f) + 10; }
     }
 }
